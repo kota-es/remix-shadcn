3648015631826298dd1e6b55a74e24706543378e
kota-es <info@k-3110.com>
2024-08-29 08:10:30 +0900

    initial commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,22 @@
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import type { MetaFunction } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <Button>Click me</Button>
+    </div>
+  );
+}
